4932be8bb0377fa792a037eceb7071d6e1b72484
Florian Weingarten <flo@hackvalue.de>
2015-05-01 17:43:25 +0000

    initial commit

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cat > ./Dockerfile <<DOCKERFILE
+FROM gliderlabs/logspout:master
+DOCKERFILE
+
+cat > ./modules.go <<MODULES
+package main
+import (
+	_ "github.com/gliderlabs/logspout/httpstream"
+	_ "github.com/gliderlabs/logspout/routesapi"
+	_ "github.com/fw42/logspout-splunk"
+)
+MODULES
+
+sudo docker build .
